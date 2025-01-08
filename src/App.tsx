edd53289087df0c9341c5e49cc9811a0dad60dff
Nguyen Duc Huy <duchuytv2102@gmail.com>
2025-01-08 23:29:07 +0700

    Setup project by vite and using TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div>
+            <p className='font-medium italic text-green-600 text-center'>Hello TypeScript</p>
+        </div>
+    );
+}
